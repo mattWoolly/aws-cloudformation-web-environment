b82acc6d20095397895b966260e46e00f909f7f1
Matt Woolly <matt@woolly.me>
2018-06-26 21:03:10 -0600

    adding testing

--- cf-control.py
+++ cf-control.py
@@ -3,18 +3,24 @@
 import boto3
 import yaml
 import argparse
+import pycurl
+from StringIO import StringIO
 
 client = boto3.client('cloudformation')
 
 cf_template = 'cloudformation_01.yml'
 template_url = 'https://s3.amazonaws.com/mattwoolly-cf-templates/cloudformation_01.yml'
 stack_name = 'web01'
+test_body = '<h1>Automation for the People</h1>'
 
 def describe_stack(name):
-    stacks = client.describe_stacks(
+    response = client.describe_stacks(
         StackName = name
     )
-    print stacks
+    stack = response['Stacks'][0]
+    for item in stack['Outputs']:
+        print item['Description'] + " : " +  item['OutputValue']
+    print ""
 
 def validate_template(template_local):
     try:
@@ -44,6 +50,29 @@ def delete_stack(name):
     except Exception as error:
         print error
 
+def test_stack(name):
+    response = client.describe_stacks(
+        StackName = name
+    )
+    stack = response['Stacks'][0]
+    for item in stack['Outputs']:
+        if item['Description'] == "Public IP":
+            url = "http://" + item['OutputValue']
+            buffer = StringIO()
+            curl = pycurl.Curl()
+            curl.setopt(curl.URL, url)
+            curl.setopt(curl.WRITEFUNCTION, buffer.write)
+            curl.perform()
+            curl.close()
+            
+            body = buffer.getvalue()
+            print "Web Server Displays: " + body.rstrip()
+            print "Web Server Should Display: " + test_body.rstrip()
+            if body.rstrip() == test_body.rstrip():
+                print "Validated"
+            else:
+                print "Not Validated"
+
 # Manual overrides
 #validate_template(cf_template)
 #create_stack(stack_name, template_url)
@@ -55,6 +84,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--describe', default=False, action='store_true',  help='Describes CloudFormation Stack')
 parser.add_argument('--create', default=False, action='store_true', help='Creates CloudFormation Stack')
 parser.add_argument('--delete', default=False, action='store_true', help='Deletes CloudFormation stack')
+parser.add_argument('--test', default=False, action='store_true', help='Tests CloudFormation stack')
 parser.add_argument('--name', required=True, help='Provide name for CloudFormation stack')
 args = parser.parse_args()
 
@@ -71,4 +101,5 @@ elif args.describe:
     describe_stack(name)
 elif args.delete:
     delete_stack(name)
-
+elif args.test:
+    test_stack(name)
